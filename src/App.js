68b0cf4e6fb594aa56a245d9f8d4ca85750eb5e5
hasaway2 <hasaway2@naver.com>
2025-06-26 17:03:23 +0900

    미저장분

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useNavigate } from 'react-router-dom';
 import './App.css';
 import { useAuthInit } from './hooks/useAuthInit';
+import { setNavigate } from './lib/navigate';
 
 function App() {
+  const navigate = useNavigate();
+
+  // useNavigate훅을 생성해서 전역 JS에 저장합니다
+  useEffect(() => {
+    setNavigate(navigate);
+  }, [navigate]);
+
+  // 로컬 스토리지의 인증 정보를 store에 복사
   useAuthInit();
-  
+
   return (
     <div className="App">
     </div>
